afea14b140c63aff08d174dfb5d8e68d5a4069df
k18rs044 <k18rs044@st.kyusan-u.ac.jp>
2019-12-03 10:08:08 +0900

    スコア

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -1,6 +1,24 @@
 // This is a JavaScript file
 
 var ncmbController = {
+  sendScore: function(score) {
+    var self = this;
+
+    // [1]Score（クラス）を生成
+    var Score = self.ncmb.DataStore("ScoreClass");
+
+    // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
+    var scoreData = new Score({score: score});
+
+    // [3]送信処理
+    scoreData.save()
+        .then(function (saved) {
+            alert("スコア送信完了！");
+        })
+       .catch(function(err){
+            console.log(err);
+        });
+},
 }
 var ncmbController={
       APPLICATION_KEY:appKey,CLIENT_KEY:clientKey,
